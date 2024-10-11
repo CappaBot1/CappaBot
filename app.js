94c2d5acebdd277420fa0cfa3e1eb245863ed984
Casper Howse <casperhowse@gmail.com>
2024-10-11 22:05:49 +1300

    Log payload

--- app.js
+++ app.js
@@ -59,15 +59,15 @@ function verifyGithub(req) {
 	if (!req.headers['user-agent'].includes('GitHub-Hookshot')) {
 		return false;
 	}
-	console.log(req.body);
+	
 	// Compare their hmac signature to our hmac signature
 	// (hmac = hash-based message authentication code)
-	/*const theirSignature = req.headers['x-hub-signature'];
+	//const theirSignature = req.headers['x-hub-signature'];
 	const payload = JSON.stringify(req.body);
-	const secret = String(process.env.GITHUB_WEBHOOK_SECRET);
-	const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
-	return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
-	*/
+	//const secret = String(process.env.GITHUB_WEBHOOK_SECRET);
+	//const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
+	//return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
+	console.log(payload)
 	return true
 }
 
