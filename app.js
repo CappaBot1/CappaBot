833d05eb84dd2ff05bb96fa928187575709b83c1
Casper Howse <casperhowse@gmail.com>
2024-10-11 22:28:03 +1300

    Just return true

--- app.js
+++ app.js
@@ -62,13 +62,13 @@ function verifyGithub(req) {
 
 	// Compare their hmac signature to our hmac signature
 	// (hmac = hash-based message authentication code)
-	const theirSignature = req.headers['x-hub-signature'];
-	const payload = JSON.stringify(req);
-	const secret = String(process.env.GITHUB_WEBHOOK_SECRET);
-	const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
-	return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
-	//console.log(payload);
-	//return true
+	//const theirSignature = req.headers['x-hub-signature'];
+	const payload = JSON.stringify(req.body);
+	//const secret = String(process.env.GITHUB_WEBHOOK_SECRET);
+	//const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
+	//return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
+	console.log(payload);
+	return true
 }
 
 /**
