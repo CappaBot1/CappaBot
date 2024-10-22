2a199f8fc61e12d50f17054d1108c2067598a696
Casper Howse <casperhowse@gmail.com>
2024-10-22 18:37:33 +1300

    Fix some formattin

--- utils.js
+++ utils.js
@@ -62,14 +62,14 @@ export function noCircular(obj) {
 	var i = 0;
 	
 	return function(key, value) {
-	  if(i !== 0 && typeof(obj) === 'object' && typeof(value) == 'object' && obj == value) 
-		return '[Circular]'; 
+	  if(i !== 0 && typeof(obj) === "object" && typeof(value) == "object" && obj == value) 
+		return "[Circular]"; 
 	  
 	  if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
-		return '[Unknown]';
+		return "[Unknown]";
 	  
 	  ++i; // so we know we aren't using the original object anymore
 	  
 	  return value;  
 	}
-  }
+}
