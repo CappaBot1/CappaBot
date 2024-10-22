320d68dd40c57f3db21913ca3ff79b2b5ff0761d
Casper Howse <casperhowse@gmail.com>
2024-10-22 17:49:15 +1300

    Prepare to add back github auth

--- app.js
+++ app.js
@@ -80,13 +80,7 @@ function verifyGithub(req) {
 		return false;
 	}
 
-	// Compare their hmac signature to our hmac signature
-	// (hmac = hash-based message authentication code)
-	//const theirSignature = req.headers['x-hub-signature'];
 	const payload = JSON.stringify(req.body);
-	//const secret = String(process.env.GITHUB_WEBHOOK_SECRET);
-	//const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
-	//return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
 	console.log(payload);
 	return true
 }
