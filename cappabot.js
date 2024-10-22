f73f3ee9c8dd10ccb20dfb931fbb3367cdf9243b
Casper Howse <casperhowse@gmail.com>
2024-10-22 21:21:55 +1300

    Remove newline

--- cappabot.js
+++ cappabot.js
@@ -192,7 +192,7 @@ To use the best command ever made (`react`) follow these instructions:\n\
 > 2. Right click on the **message** you would like to react to\n\
 > 3. Go to the `apps` dropdown\n\
 > 4. Click the `react` command next to my pfp\n\
-> 5. Wait for the image to load and enjoy!\n\n\
+> 5. Wait for the image to load and enjoy!\n\
 You can try testing it out on this message now!\
                         "
                     }
