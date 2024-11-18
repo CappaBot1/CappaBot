4f49721f262f0e22d2e4b6647a470b4461794af4
Casper Howse <casperhowse@gmail.com>
2024-11-18 21:48:55 +1300

    Change port to 3000

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ export function saveDB(callback) {
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // Define app port
-const port = 585;
+const port = 3000;
 
 // Create an express app
 const app = express();
